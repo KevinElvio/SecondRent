295befe05a577ac35f6979352cbfd53c59a65dd3
Rasthian <81855875+Rasthian@users.noreply.github.com>
2023-10-25 21:55:05 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <div class="topnav">
+
+    </div>
+   <div class="wellcome flex align-center col">
+    <div class="wellcome_content flex col justify-center">
+        <h1 class="top_h1">hi! >w<</h1>
+        <h1>Wellcome to SecondRent</h1>
+    </div>
+</div>
+    <div class="content flex col">
+        
+        <h1>Rental</h1>
+        <div class="filter flex ">
+            <a href="">
+                <div class="filter_box">
+                    <p>Top Rate</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="filter_box">
+                    <p>Top Rate</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="filter_box">
+                    <p>Top Rate</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="filter_box">
+                    <p>Top Rate</p>
+                </div>
+            </a>
+        </div>
+        <div class="content_product flex flex-wrap">
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+            <div class="content_product_card">
+                <div class="img_product flex col align-center">
+                    <img src="assets/images/a.png" alt="">
+                </div>
+               <div class="title_content">
+                <h1>
+                    name_product
+                </h1>
+                <p class="desc_content">description product</p>
+                <div class="harga flex">
+                    <p>Rp.</p><p>5000</p> 
+                </div>
+               </div>
+            
+            </div>
+        </div>
+    </div>
+   
+
+</body>
+</html>
