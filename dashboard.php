990b281070d88515d71e102ff6e29172238d0c2a
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2023-10-26 23:25:24 +0700

    add login page and command nav

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +8,48 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
 </head>
-<body>
-    <div class="topnav">
 
+<body>
+    <!-- <nav class="navbar navbar-expand-custom navbar-mainbg">
+        <a class="navbar-brand navbar-logo" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars text-white"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <div class="hori-selector">
+                    <div class="left"></div>
+                    <div class="right"></div>
+                </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Components</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Calendar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Charts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Documents</a>
+                </li>
+            </ul>
+        </div>
+    </nav> -->
+    <div class="wellcome flex align-center col">
+        <div class="wellcome_content flex col justify-center">
+            <h1 class="top_h1">Hi! </h1>
+            <h1>Wellcome to SecondRent</h1>
+        </div>
     </div>
-   <div class="wellcome flex align-center col">
-    <div class="wellcome_content flex col justify-center">
-        <h1 class="top_h1">Hi! </h1>
-        <h1>Wellcome to SecondRent</h1>
-    </div>
-</div>
     <div class="content flex col">
-        
+
         <h1>Rental</h1>
         <div class="filter flex ">
             <a href="">
@@ -26,7 +57,7 @@
                     <p>Top Rate</p>
                 </div>
             </a>
- 
+
             <a href="">
                 <div class="filter_box">
                     <p>Favorite</p>
@@ -39,72 +70,76 @@
             </a>
         </div>
         <div class="content_product flex flex-wrap">
-         <a href="">
-            <div class="content_product_card">
-                <div class="img_product flex col align-center">
-                    <img src="assets/images/443496_DTDIT_1000_001_066_0000_Light.avif" alt="">
+            <a href="">
+                <div class="content_product_card">
+                    <div class="img_product flex col align-center">
+                        <img src="assets/images/443496_DTDIT_1000_001_066_0000_Light.avif" alt="">
+                    </div>
+                    <div class="title_content">
+                        <h1>
+                            name_product
+                        </h1>
+                        <p class="desc_content">description product</p>
+                        <div class="harga flex">
+                            <p>Rp.</p>
+                            <p>5000</p>
+                        </div>
+                    </div>
+
                 </div>
-               <div class="title_content">
-                <h1>
-                    name_product
-                </h1>
-                <p class="desc_content">description product</p>
-                <div class="harga flex">
-                    <p>Rp.</p><p>5000</p> 
-                </div>
-               </div>
-            
-            </div>
-         </a>
+            </a>
             <a href="">
                 <div class="content_product_card">
                     <div class="img_product flex col align-center">
                         <img src="assets/images/download (1).jpeg" alt="">
                     </div>
-                   <div class="title_content">
-                    <h1>
-                        name_product
-                    </h1>
-                    <p class="desc_content">description product</p>
-                    <div class="harga flex">
-                        <p>Rp.</p><p>5000</p> 
+                    <div class="title_content">
+                        <h1>
+                            name_product
+                        </h1>
+                        <p class="desc_content">description product</p>
+                        <div class="harga flex">
+                            <p>Rp.</p>
+                            <p>5000</p>
+                        </div>
                     </div>
-                   </div>
-                
+
                 </div>
             </a>
-           <a href="">
-            <div class="content_product_card">
-                <div class="img_product flex col align-center">
-                    <img src="assets/images/download (2).jpeg" alt="">
+            <a href="">
+                <div class="content_product_card">
+                    <div class="img_product flex col align-center">
+                        <img src="assets/images/download (2).jpeg" alt="">
+                    </div>
+                    <div class="title_content">
+                        <h1>
+                            name_product
+                        </h1>
+                        <p class="desc_content">description product</p>
+                        <div class="harga flex">
+                            <p>Rp.</p>
+                            <p>5000</p>
+                        </div>
+                    </div>
+
                 </div>
-               <div class="title_content">
-                <h1>
-                    name_product
-                </h1>
-                <p class="desc_content">description product</p>
-                <div class="harga flex">
-                    <p>Rp.</p><p>5000</p> 
-                </div>
-               </div>
-            
-            </div>
-           </a>
+            </a>
             <a href="">
                 <div class="content_product_card">
                     <div class="img_product flex col align-center">
                         <img src="assets/images/download.jpeg" alt="">
                     </div>
-                   <div class="title_content">
-                    <h1>
-                        name_product
-                    </h1>
-                    <p class="desc_content">description product</p>
-                    <div class="harga flex">
-                        <p>Rp.</p><p>5000</p> 
+                    <div class="title_content">
+                        <h1>
+                            name_product
+                        </h1>
+                        <p class="desc_content">description product</p>
+                        <div class="harga flex">
+                            <p>Rp.</p>
+                            <p>5000</p>
+                        </div>
                     </div>
-                   </div>
-                
+
                 </div>
             </a>
             <a href="">
@@ -112,73 +147,78 @@
                     <div class="img_product flex col align-center">
                         <img src="assets/images/443496_DTDIT_1000_001_066_0000_Light.avif" alt="">
                     </div>
-                   <div class="title_content">
-                    <h1>
-                        name_product
-                    </h1>
-                    <p class="desc_content">description product</p>
-                    <div class="harga flex">
-                        <p>Rp.</p><p>5000</p> 
-                    </div>
-                   </div>
-                
-                </div>
-             </a>
-                <a href="">
-                    <div class="content_product_card">
-                        <div class="img_product flex col align-center">
-                            <img src="assets/images/download (1).jpeg" alt="">
-                        </div>
-                       <div class="title_content">
+                    <div class="title_content">
                         <h1>
                             name_product
                         </h1>
                         <p class="desc_content">description product</p>
                         <div class="harga flex">
-                            <p>Rp.</p><p>5000</p> 
+                            <p>Rp.</p>
+                            <p>5000</p>
                         </div>
-                       </div>
-                    
                     </div>
-                </a>
-               <a href="">
+
+                </div>
+            </a>
+            <a href="">
+                <div class="content_product_card">
+                    <div class="img_product flex col align-center">
+                        <img src="assets/images/download (1).jpeg" alt="">
+                    </div>
+                    <div class="title_content">
+                        <h1>
+                            name_product
+                        </h1>
+                        <p class="desc_content">description product</p>
+                        <div class="harga flex">
+                            <p>Rp.</p>
+                            <p>5000</p>
+                        </div>
+                    </div>
+
+                </div>
+            </a>
+            <a href="">
                 <div class="content_product_card">
                     <div class="img_product flex col align-center">
                         <img src="assets/images/download (2).jpeg" alt="">
                     </div>
-                   <div class="title_content">
-                    <h1>
-                        name_product
-                    </h1>
-                    <p class="desc_content">description product</p>
-                    <div class="harga flex">
-                        <p>Rp.</p><p>5000</p> 
-                    </div>
-                   </div>
-                
-                </div>
-               </a>
-                <a href="">
-                    <div class="content_product_card">
-                        <div class="img_product flex col align-center">
-                            <img src="assets/images/download.jpeg" alt="">
-                        </div>
-                       <div class="title_content">
+                    <div class="title_content">
                         <h1>
                             name_product
                         </h1>
                         <p class="desc_content">description product</p>
                         <div class="harga flex">
-                            <p>Rp.</p><p>5000</p> 
+                            <p>Rp.</p>
+                            <p>5000</p>
                         </div>
-                       </div>
-                    
                     </div>
-                </a>
-           
+
+                </div>
+            </a>
+            <a href="">
+                <div class="content_product_card">
+                    <div class="img_product flex col align-center">
+                        <img src="assets/images/download.jpeg" alt="">
+                    </div>
+                    <div class="title_content">
+                        <h1>
+                            name_product
+                        </h1>
+                        <p class="desc_content">description product</p>
+                        <div class="harga flex">
+                            <p>Rp.</p>
+                            <p>5000</p>
+                        </div>
+                    </div>
+
+                </div>
+            </a>
+
         </div>
     </div>
-   
 
+<script src="js/script.js"></script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -7,24 +7,22 @@
     <title>Second Rent - Sewa, Jual, dan Beli Barang Branded</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/login.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
 </head>
-
-<body>
-    <div class="container">
-        <form class="login_page">
-            <div class="mb-3">
-                <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-            </div>
-            <div class="mb-3">
-                <label for="exampleDropdownFormPassword2" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-            </div>
-            <div class="mb-3">
-            </div>
-            <button type="submit" class="btn btn-primary">Sign in</button>
-        </form>
-    </div>
-</body>
-
+    <body>
+        <div class="form">
+          <div class="login_box">
+            <h1>Login</h1>
+            <p>Hey there! Enter your details to get sign in to your account.</p>
+            <input type="email" placeholder="Email" class="email">
+            <input type="password" class="password" placeholder="Password" id="password">
+            <p>Having trouble in signing in? <span href="#">Click here.</span></p>
+            <button class="sign_in">Sign In</button>
+            <p>Or Sign In with</p>
+            <button><i class="fa-brands fa-google"></i> Google</button>
+            <button><i class="fa-brands fa-facebook"></i> Facebook</button>
+            <p>Don't have an account? <span href="#">Request here.</span></p>
+          </div>
+        </div>
+    </body>
 </html>
