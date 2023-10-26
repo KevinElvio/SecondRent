cde053787530a854baf37063bbbebf8fc0a9686c
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2023-10-27 00:44:27 +0700

    add about

--- about-team.php
+++ about-team.php
@@ -6,11 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Second Rent - Sewa, Jual, dan Beli Barang Branded</title>
     <link rel="stylesheet" href="css/style-about.css">
+
 </head>
+
 <body>
     <?php
     include("nav.php");
     ?>
+    <header>
+        <h1>Our Team</h1>
+    </header>
+    <div class="team-member">
+        <img src="assets/images/kevin.JPG" alt="Anggota Tim 1">
+        <h2>Kevin Elvio</h2>
+        <p>Programer</p>
+    </div>
+    <div class="team-member">
+        <img src="assets/images/rast.JPG" alt="Anggota Tim 2">
+        <h2>Rasthian Restu</h2>
+        <p>Programer</p>
+    </div>
+
+
 </body>
 
 </html>
